456c90b3d8d2ae536881fca1f924a1e932880e7c
정연우 <yoanwoo@naver.com>
2024-03-12 14:57:43 +0900

    [FE]feat: NotoSans 폰트 적용
    구글 폰트로부터 Noto sans KR 폰트를 가져왔습니다. 그리고 실제로 적용됨을 확인했습니다.

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      Notosans: ["Noto Sans KR"],
+    },
+    //fontFamily 추가
   },
   variants: {
     extend: {}
