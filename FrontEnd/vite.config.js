56ee1baa4bfa08c89aff4f007e5790fc59718fb3
정연우 <yoanwoo@naver.com>
2024-03-18 13:55:53 +0900

    [FE]fix: vite.config.js 수정

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-    host: 'j10b303.p.ssafy.io', // 원하는 IP 주소로 변경
+  server: {
     port: 3000 // 원하는 포트 번호로 변경
   },
   plugins: [
