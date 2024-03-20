774507f1b1efb23395920ae33ceac3ceb9d5ce8f
chosj33 <chosj33@naver.com>
2024-03-20 15:43:28 +0900

    [BE] feat: SearhgAPI login문제 해결 및 db수정

--- BackEnd/src/main/java/b303/farm/user/common/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ BackEnd/src/main/java/b303/farm/user/common/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -73,7 +73,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         response.addHeader(jwtService.getRefreshHeader(),"Bearer " + refreshToken);
 
         //
-        response.sendRedirect(UriComponentsBuilder.fromUriString("http://j10b303.p.ssafy.io")
+        response.sendRedirect(UriComponentsBuilder.fromUriString("http://192.168.31.164:5173")
                 .queryParam("accessToken", accessToken)
                 .queryParam("refreshToken", refreshToken)
                 .build()
