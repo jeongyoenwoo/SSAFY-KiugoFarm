7ece073061307260240f55e27d137af8217366a3
윤건웅 <dnd8997@naver.com>
2024-03-20 17:39:05 +0900

    [FE] fix: axios-connecting crops 연결
    crops 주소로 요청이 연결이 되도록 설정하였습니다.

--- FrontEnd/src/js/BaseAxios.js
+++ FrontEnd/src/js/BaseAxios.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
-const token = localStorage.getItem('token');
+const token = localStorage.getItem('accessToken');
 function localaxios () {
     const instance = axios.create({
-        baseURL: "https://j10b303.p.ssafy.io",
+        baseURL: "http://192.168.31.57:8080",
 
         headers:{
             "Content-Type": "application/json;charset=utf-8",
         },
-        "Access-Control-Allow-Origin": "https://j10b303.p.ssafy.io",
+        "Access-Control-Allow-Origin": "http://192.168.31.57:8080",
         "Access-Control-Allow-Credentials":true,
         withCredentials:true
     })
     instance.defaults.headers.common["Authorization"]=`Bearer ${token}`;
     instance.defaults.headers.post["Content-Type"]="application/json";
-    instance.defaults.headers.put["Content-Type"]="appication/json"
+    instance.defaults.headers.put["Content-Type"]="application/json"
     return instance;
 }
 
