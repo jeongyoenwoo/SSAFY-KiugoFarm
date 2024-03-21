17eb7032b6c484405e16b17ce70992d8a66c0e21
정연우 <yoanwoo@naver.com>
2024-03-21 09:44:35 +0900

    Update BaseAxios.js

--- FrontEnd/src/js/BaseAxios.js
+++ FrontEnd/src/js/BaseAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token = localStorage.getItem('accessToken');
 function localaxios () {
     const instance = axios.create({
-        baseURL: "http://j10b303.p.ssafy.io:8080",
+        baseURL: "http://j10b303.p.ssafy.io/api",
 
         headers:{
             "Content-Type": "application/json;charset=utf-8",
