e6722d607a5c6d75d538b5a0e1a9a28310ecc36f
정연우 <yoanwoo@naver.com>
2024-03-21 10:02:06 +0900

    Update SecurityConfig.java

--- BackEnd/src/main/java/b303/farm/user/config/SecurityConfig.java
+++ BackEnd/src/main/java/b303/farm/user/config/SecurityConfig.java
@@ -65,7 +65,9 @@ public class SecurityConfig {
                                 // 아이콘, css, js 관련
                                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
 //                                .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()   // 토큰 발급 경로 허용              
-                                .requestMatchers.anyRequest().permitAll();      
+                                .requestMatchers("/login").permitAll()    // 회원가입 접근 가능
+                                .requestMatchers("/crop/*").permitAll()
+                                .anyRequest().permitAll()   // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능    
                 )
 
                 /* 소셜 로그인 설정 */
