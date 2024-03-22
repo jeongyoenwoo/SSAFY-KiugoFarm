7c317157716dba0922ef5b656a0bf106057340b9
정연우 <yoanwoo@naver.com>
2024-03-22 09:54:39 +0900

    [BE]feat: MySQL 연결 설정

--- Recommend/app.py
+++ Recommend/app.py
@@ -5,13 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # MySQL 데이터베이스 설정
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ssafy@localhost/member'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ssafy@j10b303.p.ssafy.io/member'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # 데이터베이스 모델 정의
 class users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
 
@@ -19,7 +19,7 @@ class users(db.Model):
 @app.route('/users', methods=['GET'])
 def get_users():
     user = users.query.all()
-    return {'User': [{'user_id': User.user_id, 'role': User.role, 'email': User.email} for User in user]}
+    return {'User': [{'id': User.id, 'role': User.role, 'email': User.email} for User in user]}
 
 # 애플리케이션 실행
 if __name__ == '__main__':
