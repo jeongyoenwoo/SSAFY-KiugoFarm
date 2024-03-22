6d624840a46c345e4fb8edabf0ed8b081fb2bb0a
정연우 <yoanwoo@naver.com>
2024-03-22 09:57:04 +0900

    Update app.py

--- Recommend/app.py
+++ Recommend/app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # 데이터베이스 모델 정의
-class users(db.Model):
+class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
@@ -18,8 +18,8 @@ class users(db.Model):
 # 라우트 및 API 엔드포인트 정의
 @app.route('/users', methods=['GET'])
 def get_users():
-    user = users.query.all()
-    return {'User': [{'id': User.id, 'role': User.role, 'email': User.email} for User in user]}
+    user_info = user.query.all()
+    return {'User': [{'id': User.id, 'role': User.role, 'email': User.email} for User in user_info]}
 
 # 애플리케이션 실행
 if __name__ == '__main__':
