04c0df53e67bb2c6ef2d52795dea9d6eb12dc4f8
정연우 <yoanwoo@naver.com>
2024-03-25 10:35:53 +0900

    [BE]feat: Recipe DB 접근 테스트9
    테스트여

--- Recommend/app.py
+++ Recommend/app.py
@@ -23,19 +23,23 @@ class Crop(db.Model):
 def calculate_cosine_similarity(liked_crop, crops):
     tfidf_vectorizer = TfidfVectorizer()
 
-    # 좋아요를 누른 작물을 제외한 데이터를 피처로 벡터화
-    crop_features = [crop["temperature"] + crop["sunshine"] + crop["water_period"] for crop in crops[:-1]]
+    crop_features = [crop["temperature"] + crop["sunshine"] + crop["water_period"] for crop in crops if
+                     crop["id"] != liked_crop["id"]]
+
+    # 좋아요를 누른 작물의 피처를 벡터화
     liked_crop_feature = liked_crop["temperature"] + liked_crop["sunshine"] + liked_crop["water_period"]
+    liked_crop_vector = tfidf_vectorizer.transform([liked_crop_feature])
 
     # 벡터화된 피처들에 대해 코사인 유사도 계산
     tfidf_matrix = tfidf_vectorizer.fit_transform(crop_features)
-    liked_crop_vector = tfidf_vectorizer.transform([liked_crop_feature])
     cosine_similarities = cosine_similarity(liked_crop_vector, tfidf_matrix)
 
-    # 코사인 유사도가 가장 높은 상위 n개의 농작물 추천
-    n_recommendations = 3  # 추천할 농작물 개수
-    similar_crops_indices = cosine_similarities.argsort()[0][::-1]  # 유사도가 높은 순으로 정렬
-    recommended_crops = [crops[i] for i in similar_crops_indices][:n_recommendations]  # 상위 n개 선택
+    # 코사인 유사도가 가장 높은 작물 추천
+    similar_crops_indices = cosine_similarities.argsort()[0][::-1]
+    recommended_crops = []
+    for index in similar_crops_indices:
+        recommended_crops.append(crops[index])
+
     return recommended_crops
 
 
