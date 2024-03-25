1f2d81800acc01a021a9e5fd6798ee9b37e814ae
정연우 <yoanwoo@naver.com>
2024-03-25 10:19:45 +0900

    [BE]feat: Recipe DB 접근 테스트5
    테스트여

--- Recommend/app.py
+++ Recommend/app.py
@@ -68,9 +68,9 @@ def get_recommended_crop():
         })
 
     # 코사인 유사도를 사용하여 추천 농작물 계산
-    # recommended_crop = calculate_cosine_similarity(liked_crop, crops)
+    recommended_crop = calculate_cosine_similarity(liked_crop, crops)
 
-    return jsonify({"recommended_crop": crops})
+    return jsonify({"recommended_crop": recommended_crop})
 
 # 애플리케이션 실행
 if __name__ == '__main__':
