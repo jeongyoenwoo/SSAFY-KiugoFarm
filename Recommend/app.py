f0c2453f46fb9163634169c16aa5d9dd57b1eb19
정연우 <yoanwoo@naver.com>
2024-03-25 14:49:33 +0900

    [BE]feat: Recipe DB 접근 테스트24
    테스트여

--- Recommend/app.py
+++ Recommend/app.py
@@ -92,31 +92,21 @@ def calculate_cosine_similarity(liked_crops, crops):
                                 string_to_number(crop["water_exit"])])
         # 각 작물의 벡터와 좋아요를 누른 작물들의 벡터 사이의 코사인 유사도 계산
         cosine_similarities = cosine_similarity(liked_crop_features, crops)
-        # similarity = np.dot(liked_crop_features, crop_vector) / (
-        #         np.linalg.norm(liked_crop_features, axis=1) * np.linalg.norm(crop_vector))
-        # cosine_similarities.append(similarity)
 
-    # 각 작물과의 유사도를 계산한 후 상위 3개를 추출
-    recommended_crops_indices = np.argsort(cosine_similarities, axis=1)[:, ::-1][:, :3]
-    recommended_crops = []
-    for indices in recommended_crops_indices:
-        recommended_crops.append([crops[index] for index in indices])
+    similar_crops_indices = np.argsort(cosine_similarities, axis=1)[:, ::-1]
+    unique_indices = []
+    for indices in similar_crops_indices:
+        for index in indices:
+            if index not in unique_indices:
+                unique_indices.append(index)
+            if len(unique_indices) == 3:  # 중복을 제거한 후 상위 3개의 작물만 추천
+                break
+        if len(unique_indices) == 3:
+            break
+
+    recommended_crops = [crops[index] for index in unique_indices]
 
     return recommended_crops
-    # similar_crops_indices = np.argsort(cosine_similarities, axis=1)[:, ::-1]
-    # unique_indices = []
-    # recommended_crops = []
-    # for indices in similar_crops_indices:
-    #     for index in indices:
-    #         if index not in unique_indices:
-    #             unique_indices.append(index)
-    #             recommended_crops.append(crops[index])
-    #         if len(unique_indices) == 3:  # 중복을 제거한 후 상위 3개의 작물만 추천
-    #             break
-    #     if len(unique_indices) == 3:
-    #         break
-    #
-    # return recommended_crops
 
 
 # API 엔드포인트 정의
