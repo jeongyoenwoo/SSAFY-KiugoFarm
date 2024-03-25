0f5f7801749b5c76ff817b81a102c82eb64e8214
정연우 <yoanwoo@naver.com>
2024-03-25 13:54:33 +0900

    [BE]feat: Recipe DB 접근 테스트19
    테스트여

--- Recommend/app.py
+++ Recommend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 # Flask 애플리케이션 생성
 app = Flask(__name__)
@@ -65,49 +66,39 @@ def season_to_number(value):
     else:
         raise ValueError("Invalid value: {}".format(value))
 
+
 # 코사인 유사도 계산 함수
 def calculate_cosine_similarity(liked_crops, crops):
-    tfidf_vectorizer = TfidfVectorizer()
-
-    crop_features = [string_to_number(crop["temperature"]) + string_to_number(crop["sunshine"]) +
-                     string_to_number(crop["water_period"]) + difficulty_to_number(crop["difficulty"]) +
-                     string_to_number(crop["grow_time"]) + string_to_number(crop["humidity"]) +
-                     season_to_number(crop["grow_start"]) + string_to_number(crop["water_exit"])
-                     for crop in crops if crop["id"] not in [liked_crop["id"] for liked_crop in liked_crops]]
+    liked_crop_features = np.array([[string_to_number(liked_crop["temperature"]),
+                                     string_to_number(liked_crop["sunshine"]),
+                                     string_to_number(liked_crop["water_period"]),
+                                     difficulty_to_number(liked_crop["difficulty"]),
+                                     string_to_number(liked_crop["grow_time"]),
+                                     string_to_number(liked_crop["humidity"]),
+                                     season_to_number(liked_crop["grow_start"]),
+                                     string_to_number(liked_crop["water_exit"])]
+                                    for liked_crop in liked_crops])
 
     # 좋아요를 누른 작물의 피처를 벡터화
-    liked_crop_features = []
-    for liked_crop in liked_crops:
-        liked_crop_feature = [string_to_number(liked_crop["temperature"]),
-                              string_to_number(liked_crop["sunshine"]),
-                              string_to_number(liked_crop["water_period"]),
-                              difficulty_to_number(liked_crop["difficulty"]),
-                              string_to_number(liked_crop["grow_time"]),
-                              string_to_number(liked_crop["humidity"]),
-                              season_to_number(liked_crop["grow_start"]),
-                              string_to_number(liked_crop["water_exit"])]
-        liked_crop_features.append(liked_crop_feature)
+    cosine_similarities = []
+    for crop in crops:
+        crop_vector = np.array([string_to_number(crop["temperature"]),
+                                string_to_number(crop["sunshine"]),
+                                string_to_number(crop["water_period"]),
+                                difficulty_to_number(crop["difficulty"]),
+                                string_to_number(crop["grow_time"]),
+                                string_to_number(crop["humidity"]),
+                                season_to_number(crop["grow_start"]),
+                                string_to_number(crop["water_exit"])])
+        # 각 작물의 벡터와 좋아요를 누른 작물들의 벡터 사이의 코사인 유사도 계산
+        similarity = np.dot(liked_crop_features, crop_vector) / (
+                    np.linalg.norm(liked_crop_features) * np.linalg.norm(crop_vector))
+        cosine_similarities.append(similarity)
 
-        liked_crop_vectors = tfidf_vectorizer.fit_transform(liked_crop_features)
-
-    # TF-IDF vectorizer가 학습되었는지 확인
-    if not hasattr(tfidf_vectorizer, 'vocabulary_'):
-        raise ValueError("TF-IDF vectorizer is not fitted.")
-
-    # TF-IDF 변환 후에도 벡터화된 데이터가 있는지 확인
-    if liked_crop_vectors is None:
-        raise ValueError("Liked crop vector is None after TF-IDF transformation.")
-
-    # 벡터화된 피처들에 대해 코사인 유사도 계산
-    tfidf_matrix = tfidf_vectorizer.transform(crop_features)
-    cosine_similarities = cosine_similarity(liked_crop_vectors, tfidf_matrix)
-
-    # 코사인 유사도가 가장 높은 작물 추천 (상위 3개)
-    similar_crops_indices = cosine_similarities.argsort()[0][::-1][:3]
+    similar_crops_indices = np.argsort(cosine_similarities)[::-1][:3]
     recommended_crops = [crops[index] for index in similar_crops_indices]
 
     return recommended_crops
-
 
 
 # API 엔드포인트 정의
@@ -174,6 +165,7 @@ def get_recommended_crop():
     recommended_crop = calculate_cosine_similarity(liked_crops, crops)
 
     return jsonify({"recommended_crop": recommended_crop})
+
 
 # 애플리케이션 실행
 if __name__ == '__main__':
