70201399c594c9214380d4fefef12b60356a7795
정연우 <yoanwoo@naver.com>
2024-03-27 16:15:11 +0900

    [FE]feat: 마커 클릭 시 정보창이 나오도록 구현
    마커를 클릭하면 해당 텃밭에 대한 정보가 나올 화면을 구현하였다. absolute 이기 때문에 항상 오른쪽에 나오게 되고 닫기 버튼을 클릭하면 사라진다. 해당 창에는 텃밭 정보와 찜하기 버튼, 상세정보 버튼이 있다.

--- FrontEnd/src/components/garden/GardenComponent.vue
+++ FrontEnd/src/components/garden/GardenComponent.vue
@@ -1,21 +1,63 @@
 <template>
   <div class="section">
-    <div class="mb-8 flex flex-row font-Notosans">
-      <div class="pt-[132px]  text-sm">운영주체</div>
-      <div class="pt-32 text-xl text-[#8F8F8D] ml-3">지자체</div>
-      <div class="pt-32 text-xl text-[#8F8F8D] ml-3">민간단체</div>
-      <div class="pt-32 text-xl text-[#8F8F8D] ml-3">개인</div>
-      <div class="pt-32 text-xl ml-3">모두 표시</div>
-      <div class="pt-32 text-xl ml-8">
-        <input type="text"  class="w-56 h-8 bg-[#F0F0F0] rounded-md text-sm pl-3" placeholder="지역명 검색"/>
+
+<!--    <div class="mb-8 flex flex-row font-Notosans">-->
+<!--      <div class="pt-[132px]  text-sm">운영주체</div>-->
+<!--      <div class="pt-32 text-xl text-[#8F8F8D] ml-3">지자체</div>-->
+<!--      <div class="pt-32 text-xl text-[#8F8F8D] ml-3">민간단체</div>-->
+<!--      <div class="pt-32 text-xl text-[#8F8F8D] ml-3">개인</div>-->
+<!--      <div class="pt-32 text-xl ml-3">모두 표시</div>-->
+<!--      <div class="pt-32 text-xl ml-8">-->
+<!--        <input type="text" class="w-56 h-8 bg-[#F0F0F0] rounded-md text-sm pl-3" placeholder="지역명 검색" />-->
+<!--      </div>-->
+<!--    </div>-->
+
+    <!--지도-->
+    <div id="map" class="absolute left-0 top-32 w-full h-[80%] z-10"></div>
+
+    <!--정보창-->
+    <div v-if="isDivVisible" class="absolute top-32 right-0 w-[23%] h-[80%] bg-white z-20 font-Notosans">
+      <img src="@/assets/close.svg" class="h-8 w-8 absolute right-3 top-3 cursor-pointer" @click="handleMarkerClick">
+      <div class="flex  w-full h-[32%] bg-blue-200 ">
+        <img src="@/assets/garden.png" class="object-cover w-full h-full">
+      </div>
+
+      <div class="flex flex-col items-center">
+        <span class="font-bold text-xl mt-6">동구행복나눔텃밭</span>
+        <span class="font-light text-base text-[#696969] mt-3">대구광역시 동구 불로동 823-12</span>
+      </div>
+
+      <div class="flex flex-col ml-[15%]">
+        <div class="flex flex-row mt-10">
+          <span class="font-medium text-base">운영주체</span>
+          <span class="font-light text-[#696969] text-base ml-6">지자체</span>
+        </div>
+        <div class="flex flex-row mt-4">
+          <span class="font-medium text-base">부대시설</span>
+          <span class="font-light text-[#696969] text-base ml-6">주차장, 화장실</span>
+        </div>
+        <div class="flex flex-row mt-4">
+          <span class="font-medium text-base">신청방법</span>
+          <span class="font-light text-[#696969] text-base ml-6">방문</span>
+        </div>
+        <div class="flex flex-row mt-4">
+          <span class="font-medium text-base">분양가격</span>
+          <span class="font-light text-[#696969] text-base ml-6">미정</span>
+        </div>
+      </div>
+
+      <div class="flex flex-row mt-[18%] justify-center" >
+        <div class="cursor-pointer border-[1px] border-[#EC6B6B] text-[#EC6B6B]  rounded-lg w-28 h-12 flex flex-row items-center justify-center ">
+          <img src="@/assets/heart.svg" class="h-6 w-6 mr-1.5">
+          찜하기
+        </div>
+        <div class="cursor-pointer ml-6 w-36 h-12 bg-[#86BF60] text-white  rounded-lg flex items-center justify-center">상세정보</div>
       </div>
     </div>
-    <div id="map" class="absolute left-0.5 w-full h-[75%]"></div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "test",
   data() {
@@ -23,8 +65,9 @@ export default {
       map: null,
       markers: [],
       latitude: 0,
-      longitude: 0
-    }
+      longitude: 0,
+      isDivVisible: false
+    };
   },
   created() {
     if (!("geolocation" in navigator)) {
@@ -32,27 +75,28 @@ export default {
     }
 
     // get position
-    navigator.geolocation.getCurrentPosition(pos => {
-      this.latitude = pos.coords.latitude;
-      this.longitude = pos.coords.longitude;
+    navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.latitude = pos.coords.latitude;
+          this.longitude = pos.coords.longitude;
 
-      if (window.kakao && window.kakao.maps) {
-
-        this.initMap();
-
-      } else {
-        const script = document.createElement("script");
-        /* global kakao */
-        script.onload = () => kakao.maps.load(this.initMap);
-        script.src = "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=7d1ba57eb57e1922b3275d2bcb6791e8";
-        document.head.appendChild(script);
-      }
-
-    }, err => {
-      alert(err.message);
-    })
+          if (window.kakao && window.kakao.maps) {
+            this.initMap();
+          } else {
+            const script = document.createElement("script");
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src =
+                "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=7d1ba57eb57e1922b3275d2bcb6791e8";
+            document.head.appendChild(script);
+          }
+        },
+        (err) => {
+          alert(err.message);
+        }
+    );
   },
   methods: {
+
     initMap() {
       const container = document.getElementById("map");
       const options = {
@@ -65,6 +109,7 @@ export default {
     displayMarker(markerPositions) {
       if (this.markers.length > 0) {
         this.markers.forEach((marker) => marker.setMap(null));
+        this.markers = [];
       }
 
       const positions = markerPositions.map(
@@ -72,12 +117,11 @@ export default {
       );
 
       if (positions.length > 0) {
-        this.markers = positions.map(
-            (position) =>
-                new kakao.maps.Marker({
-                  map: this.map,
-                  position,
-                })
+        this.markers = positions.map((position) =>
+            new kakao.maps.Marker({
+              map: this.map,
+              position,
+            })
         );
 
         const bounds = positions.reduce(
@@ -86,12 +130,53 @@ export default {
         );
 
       }
-      this.map.setBounds(bounds);
-    }
-  }
-}
-</script>
+      const newMarkers = [
+        {
+          title: "카카오",
+          latlng: new kakao.maps.LatLng(36.3744745, 127.2965807),
+        },
+        {
+          title: "생태연못",
+          latlng: new kakao.maps.LatLng(36.3331874, 127.2967792),
+        },
+        {
+          title: "텃밭",
+          latlng: new kakao.maps.LatLng(36.3387924, 127.2849865),
+        },
+        {
+          title: "근린공원",
+          latlng: new kakao.maps.LatLng(33.451393, 126.570738),
+        },
+      ];
 
+      const imageSrc =
+          "https://i.ibb.co/ch1GjBH/marker.png";
+
+      for (let i = 0; i < newMarkers.length; i++) {
+        let imageSize = new kakao.maps.Size(40, 54);
+        let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+        let marker = new kakao.maps.Marker({
+          map: this.map, // 수정: this.map으로 변경
+          position: newMarkers[i].latlng,
+          title: newMarkers[i].title,
+          image: markerImage,
+        });
+
+        kakao.maps.event.addListener(marker, "click", () => {
+          // 클릭된 마커에 대한 처리를 수행하는 함수 호출
+          this.handleMarkerClick(newMarkers[i]);
+        });
+      }
+      this.map.setBounds(bounds);
+    },
+    handleMarkerClick() {
+      this.isDivVisible = !this.isDivVisible;
+    }
+  },
+
+};
+</script>
 
 <style scoped>
 .test {
@@ -101,5 +186,4 @@ export default {
   align-items: center;
   height: 100vh;
 }
-
 </style>
