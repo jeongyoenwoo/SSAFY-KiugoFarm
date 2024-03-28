cc43b1a1b891ca72d35324647fe00491dc8bfecc
정연우 <yoanwoo@naver.com>
2024-03-28 10:12:05 +0900

    [FE]feat: 좋아요 기반 농작물 추천 기능 구현
    메인 페이지의 농작물 추천 탭에서 사용자의 좋아요 데이터를 기반으로 3개의 농작물을 추천해준다. 각 농작물을 클릭해 해당 농작물의 상세 정보 페이지로 넘어갈 수 있다.

--- FrontEnd/src/components/home/sections/HomeRecommendationSection.vue
+++ FrontEnd/src/components/home/sections/HomeRecommendationSection.vue
@@ -10,31 +10,51 @@
                 class="recommendation-vegetable-list delay-animate"
                 style="justify-content: space-evenly; width: 60%; margin-bottom: 3%;"
             >
-                <v-col v-for="(vegetable, index) in vegetableList" :key="index" style="margin-left: 5%;">
-                    <div style="display: flex; justify-content: center;">
-                        <img :src="vegetable.src" alt="Vegetable image" style="width: 60%;">
+                <v-col v-for="(crop, index) in cropRecommends" :key="index" style="margin-left: 5%;">
+                    <div style="display: flex; justify-content: center;" class="cursor-pointer" @click="router.push(`/search/${crop.id}`)">
+                      <v-avatar size="160">
+                        <v-img :src="crop.image_url" alt="" class="object-cover"></v-img>
+                      </v-avatar>
                     </div>
-                    <p style="font-size: 22px; margin-bottom: 10px;  font-weight: bold;">{{ vegetable.name }}</p>
-                    <p style="font-size: 15px;">{{ vegetable.introduction }}</p>
+                    <p class="font-Notosans font-medium text-xl mt-3">{{ crop.name }}</p>
                 </v-col>
             </v-row>
 
-            <div 
-                class="recommendation-button delay-animate"
-            >더 많은 추천 받으러 가기 →
-            </div>
+          <router-link to="/recommendation" class="recommendation-button delay-animate">
+            더 많은 추천 받으러 가기 →
+          </router-link>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {onMounted, ref} from 'vue';
+import axios from "axios";
+import {useRouter} from "vue-router";
+
+const router = useRouter()
 
 const vegetableList = ref([
     { src: '/vegetable/vegetable_1.png', name: '청치마상추', introduction: "우리나라에서만 주로 재배되는'청치마상추'입니다. 맛이 순하고 잎이 부드러워서 어린이도 먹기 좋죠.우리 가족을 위한 건강한 쌈채소입니다."},
     { src: '/vegetable/vegetable_2.png', name: '방울토마토', introduction: "방울토마토는 일반 토마토보다 당도가 높고 영양성분은 더 풍부하게 들어 있는 과채류 숙성채소예요. 토마토보다 먹기에 더 간편하고, 주식보다는 간식이나 후식으로 많이 이용합니다."},
     { src: '/vegetable/vegetable_3.png', name: '겨자채', introduction: "톡 쏘는 매운맛이 그리울 때, 겨자채의 알싸한 향을 즐겨보세요.조금 부드럽게 맛보고 싶다면, 여린 잎일 때 먹는 것도 추천합니다."},
 ])
+const cropRecommends = ref([]);
+
+async function fetchCropRecommends() {
+  try {
+    const response = await axios.get('https://j10b303.p.ssafy.io/recommendapi/crop/1');
+    cropRecommends.value = response.data.recommended_crop;
+    console.log(cropRecommends.value);
+  } catch (error) {
+    console.error('Error fetching recipe favorites:', error);
+  }
+}
+
+onMounted(() => {
+  fetchCropRecommends();
+});
+
 
 </script>
 
