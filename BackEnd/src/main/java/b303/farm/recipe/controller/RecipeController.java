45fb38f81148d19654ac1aecdbed4299e574c1b3
Sooji Kwon <gkdrhd6788@gmail.com>
2024-04-01 09:40:37 +0900

    [BE]feat:getRecipeListByKeyword 스트링->recipeId로 변경
    이전에 develop에 반영 안된것 다시 반영

--- BackEnd/src/main/java/b303/farm/recipe/controller/RecipeController.java
+++ BackEnd/src/main/java/b303/farm/recipe/controller/RecipeController.java
@@ -106,10 +106,37 @@ public class RecipeController {
         }
         return ResponseEntity.ok().body(recipeDtoList);
     }
+//지우지 마세요!!
+//    @GetMapping("/search/{keyword}")
+//    public ResponseEntity<List<RecipeDto>> getRecipeByKeyword(@PathVariable String keyword) {
+//        List<Recipe> recipeList = recipeService.getRecipeListByKeyword(keyword);
+//        List<RecipeDto> recipeDtoList = new ArrayList<>();
+//
+//        log.info("해당 제목을 포함한 레시피 정보");
+//        for(Recipe r : recipeList) {
+//            RecipeDto recipeDto = RecipeDto.builder()
+//                    .id(r.getId())
+//                    .serialNum(r.getSerialnum())
+//                    .name(r.getName())
+//                    .info(r.getInfo())
+//                    .ingredients(r.getIngredients())
+//                    .cook(r.getCook())
+//                    .imageUrl(r.getImage_url())
+//                    .difficulty(r.getDifficulty())
+//                    .likes(r.getLikes())
+//                    .cookTime(r.getCookTime())
+//                    .servings(r.getServings())
+//                    .build();
+//            log.info("레시피 정보: {}", recipeDto);
+//            recipeDtoList.add(recipeDto);
+//        }
+//        return ResponseEntity.ok().body(recipeDtoList);
+//    }
 
-    @GetMapping("/search/{keyword}")
-    public ResponseEntity<List<RecipeDto>> getRecipeByKeyword(@PathVariable String keyword) {
-        List<Recipe> recipeList = recipeService.getRecipeListByKeyword(keyword);
+    //수지 테스트
+    @GetMapping("/searchByCropId/{cropId}")
+    public ResponseEntity<List<RecipeDto>> getRecipeByCropId(@PathVariable Long cropId) {
+        List<Recipe> recipeList = recipeService.getRecipeListByKeyword(cropId);
         List<RecipeDto> recipeDtoList = new ArrayList<>();
 
         log.info("해당 제목을 포함한 레시피 정보");
